ba8f6b67cb47e423f9e815c90a200dbc4c3246b8
gustavosrxdvhbj <143908490+gustavosrxdvhbj@users.noreply.github.com>
2025-08-16 04:51:54 -0400

    Update script.js

--- Auris copy/script.js
+++ Auris copy/script.js
@@ -1,8 +1,32 @@
+//------------------------------------------------------------------------------o
+//                                     SPECTREE                                 |
+//------------------------------------------------------------------------------o 
+
+// Botões fixos do site (não dependem do DOMContentLoaded)
+let seeprod = document.querySelector('#see'); 
+let aacd = document.querySelector('#siteacd'); 
+let amazonia = document.querySelector('#amazon'); 
+
+if (seeprod) {
+    seeprod.onclick = () => { window.location = 'produtos.html'; };
+}
+if (aacd) {
+    aacd.onclick = () => { window.location = 'https://aacd.org.br/'; };
+}
+if (amazonia) {
+    amazonia.onclick = () => { window.location = 'https://www.fundoamazonia.gov.br/pt/home/'; };
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    // Inicializa o EmailJS (se a lib estiver carregada)
+    if (typeof emailjs !== "undefined") {
+        emailjs.init("vMBOBTs3yiE98J3bh");
+    }
+
+    // -------------------------------------------------------------------------
+    // Slideshow automático
     const slides = document.querySelectorAll('.slideshow img');
     let current = 0;
-
-    // Slide automático
     if (slides.length > 0) {
         setInterval(() => {
             slides[current].classList.remove('active');
@@ -11,21 +35,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 3000);
     }
 
-    // Página produtos.html: redireciona para pay.html com alerta
+    // -------------------------------------------------------------------------
+    // Página produtos.html: redireciona para pay.html
     const productLinks = document.querySelectorAll('a[id]');
     if (productLinks.length > 0) {
         productLinks.forEach(el => {
             el.addEventListener('click', (e) => {
-                e.preventDefault(); // evita o href padrão
-                const id = e.target.id;
-
-                // Redireciona para pay.html passando o id
-                window.location.href = `pay.html?id=${id}`;
+                if (el.id.match(/^\d+$/)) {
+                    e.preventDefault();
+                    window.location.href = `pay.html?id=${el.id}`;
+                }
             });
         });
     }
 
-    // Página pay.html: altera a imagem e mostra texto + QR Code centralizado
+    // -------------------------------------------------------------------------
+    // Página pay.html: mostra produto comprado + QR Code
     const produtoImg = document.getElementById('produto-img');
     const texto = document.querySelector('#paragra');
 
@@ -33,46 +58,42 @@ document.addEventListener("DOMContentLoaded", function () {
         const params = new URLSearchParams(window.location.search);
         const id = params.get('id');
 
-        // Limpa conteúdo anterior
         texto.innerHTML = "";
         let mensagem = "";
         let linkQRCode = "";
 
-        switch(id) {
+        switch (id) {
             case '1':
                 produtoImg.src = 'minha_imagem.jpeg';
-                mensagem = "Você acaba de comprar o conjunto de diamantes<br>Valor: 3,000R$<br>Valor destinado à doação: 1,000R$<br>Colaborando com Fundo Amazonia<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
+                mensagem = "Você acaba de comprar o conjunto de diamantes<br>Valor: 3,000R$<br>Valor destinado à doação: 1,000R$<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
                 linkQRCode = "https://seusite.com/pagamento?id=1";
                 break;
             case '2':
                 produtoImg.src = 'joiasvermelhas.jpeg';
-                mensagem = "Você acaba de comprar o conjunto de rubis<br>Valor: 10,000R$<br>Valor destinado à doação: 4,000R$<br>Colaborando com AACD<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
+                mensagem = "Você acaba de comprar o conjunto de rubis<br>Valor: 10,000R$<br>Valor destinado à doação: 4,000R$<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
                 linkQRCode = "https://seusite.com/pagamento?id=2";
                 break;
             case '3':
                 produtoImg.src = 'joiasverdes.jpeg';
-                mensagem = "Você acaba de comprar o conjunto de esmeraldas<br>Valor: 9,000R$<br>Valor destinado à doação: 2,000R$<br>Colaborando com Fundo Amazonia<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
+                mensagem = "Você acaba de comprar o conjunto de esmeraldas<br>Valor: 9,000R$<br>Valor destinado à doação: 2,000R$<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
                 linkQRCode = "https://seusite.com/pagamento?id=3";
                 break;
             case '4':
                 produtoImg.src = 'perolas.jpeg';
-                mensagem = "Você acaba de comprar o conjunto de pérolas e ouro<br>Valor: 8,000R$<br>Valor destinado à doação: 3,000R$<br>Colaborando com AACD<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
+                mensagem = "Você acaba de comprar o conjunto de pérolas e ouro<br>Valor: 8,000R$<br>Valor destinado à doação: 3,000R$<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
                 linkQRCode = "https://seusite.com/pagamento?id=4";
                 break;
             case '5':
                 produtoImg.src = 'joiasroxas.jpeg';
-                mensagem = "Você acaba de comprar o conjunto de diamantes roxo<br>Valor: 100,000R$<br>Valor destinado à doação: 20,000R$<br>Colaborando com Fundo amazonia<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
+                mensagem = "Você acaba de comprar o conjunto de diamantes roxo<br>Valor: 100,000R$<br>Valor destinado à doação: 20,000R$<br>Infelizmente só estamos com a função de PIX habilitada atualmente<br>";
                 linkQRCode = "https://seusite.com/pagamento?id=5";
                 break;
             default:
                 produtoImg.src = 'variavel.jpeg';
-                mensagem = "";
         }
 
-        // Adiciona mensagem + container do QR Code
         if (mensagem !== "") {
             texto.innerHTML = mensagem + "<div id='qrcode' style='margin-top: 20px; display: flex; justify-content: center;'></div>";
-            // Gera o QR Code
             new QRCode(document.getElementById("qrcode"), {
                 text: linkQRCode,
                 width: 150,
@@ -80,22 +101,21 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     }
+
+    // -------------------------------------------------------------------------
+    // Formulário (cadastro) com EmailJS
+    const form = document.getElementById('meu-form');
+    if (form && typeof emailjs !== "undefined") {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            emailjs.sendForm('service_cufvvha', 'template_lvt28q8', this)
+                .then(function () {
+                    console.log('Email enviado com sucesso!');
+                    window.location.href = 'aguardar.html';
+                }, function (error) {
+                    console.error('Erro ao enviar email:', error);
+                    alert('Ocorreu um erro ao enviar. Tente novamente.');
+                });
+        });
+    }
 });
-
-//------------------------------------------------------------------------------o
-//                                     SPECTREE                                 |
-//------------------------------------------------------------------------------o 
-let seeprod = document.querySelector('#see'); 
-let aacd = document.querySelector('#siteacd'); 
-let amazonia = document.querySelector('#amazon'); 
-
-seeprod.onclick = () => {
-    window.location = 'produtos.html'; 
-};
-aacd.onclick = () => { 
-    window.location = 'https://aacd.org.br/'; 
-};
-amazonia.onclick = () => { 
-    window.location = 'https://www.fundoamazonia.gov.br/pt/home/'; 
-};
-
